12c57853cb2c7a2e79b74b0ff0f0433d53ec0a8a
tonytruong101 <ttruong@rennie.com>
2023-03-12 17:33:02 -0700

    updated readme

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pyfiglet
 import json
 import openai
 
-openai.api_key = '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'
+openai.api_key = ''
 
 # Set the text to be displayed
 text = "DOCKERBOT"
