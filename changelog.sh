5bdd828145a00038ff7f95d5aa506e9e09611240
tonytruong101 <100802811+tonytruong101@users.noreply.github.com>
2023-03-12 19:28:11 -0700

    Update changelog.sh
    made it more readible

--- changelog.sh
+++ changelog.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-git log --pretty=format:"%cd%n- %h %s (%an)%n" --date=short | awk 'BEGIN { FS="\n"; OFS="\n" } { if ($1 != p) { if (title != "") print entries; title=$1; entries=""title"\n"; first_commit=1 }; p=$1; $1="## "; if (!first_commit) entries=entries"\n"; entries=entries""$0; first_commit=0 } END { if (title != "") print entries }' | awk '!a[$0]++' > CHANGELOG.md
+
+git log --pretty=format:"%cd%n- %h %s (%an)%n" --date=short \
+| awk 'BEGIN { FS="\n"; OFS="\n" } \
+    { \
+        if ($1 != p) { \
+            if (title != "") \
+                print entries; \
+            title=$1; \
+            entries=""title"\n"; \
+            first_commit=1 \
+        }; \
+        p=$1; \
+        $1="## "; \
+        if (!first_commit) \
+            entries=entries"\n"; \
+        entries=entries""$0; \
+        first_commit=0 \
+    } \
+    END { \
+        if (title != "") \
+            print entries \
+    }' \
+| awk '!a[$0]++' \
+> CHANGELOG.md
 
 
 
