41b230be5068419483271cd3dc1c6e6d534094e9
tonytruong101 <ttruong@rennie.com>
2023-03-13 17:58:34 -0700

    fixed version validation to accept lts and latest

--- validations/version_validation.py
+++ validations/version_validation.py
@@ -9,8 +9,9 @@ def validate_version(version, exact_match=False):
         valid_versions = [f"{i}" for i in range(1, 100)]
         valid_versions += [f"{i}.{j}" for i in range(1, 100) for j in range(1, 100)]
         valid_versions += [f"{i}.{j}.*" for i in range(1, 100) for j in range(1, 100)]
+        valid_versions += ["lts", "latest"]
 
     while not any(fnmatch.fnmatch(version, v) for v in valid_versions):
-        version = input(f"Invalid input. Please enter a valid option from the list ({', '.join(valid_versions)}): ")
+        version = input(f"Invalid input. Please enter a valid option or format (lts, latest, x, x.x, x.x.x): ")
     return version
 
